8f2deebc2305cdba8fe317117a80f679d861b76e
daryolo97 <46422669+daryolo97@users.noreply.github.com>
2020-04-21 12:13:07 +0200

    Update IDAstar.pl

--- prolog/IDAstar.pl
+++ prolog/IDAstar.pl
@@ -51,10 +51,3 @@ dfs_aux(S, _, Visitati, _):-
   assert(minF(F)),
   !, fail.
 
-manhattan(pos(Riga, Colonna), Distanza):-
-  finale(pos(RigaFinale, ColonnaFinale)),
-  DistanzaRigaTemp is RigaFinale - Riga,
-  DistanzaRiga is abs(DistanzaRigaTemp),
-  DistanzaColonnaTemp is ColonnaFinale - Colonna,
-  DistanzaColonna is abs(DistanzaColonnaTemp),
-  Distanza is DistanzaRiga + DistanzaColonna.
